0e28deda8d49e58d6455cc9116916c7d9abc48e5
chrissdks <cd.kaegui@acad.ucb.edu.bo>
2017-04-20 00:33:05 -0700

    seeder de usuario

--- database/seeds/users_seeder.php
+++ database/seeds/users_seeder.php
@@ -15,7 +15,7 @@ class users_seeder extends Seeder
     {
         $faker = Faker::create();
         DB::table('users')->insert([
-            ['name' => $faker->firstNameFemale,
+            ['name' => $faker->firstNameMale,
             'last_name' => $faker->lastName,
             'email' => str_random(10).'@gmail.com',
             'password' => bcrypt('secret'),
