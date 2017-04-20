5c5e1f81518d65939c2bb62088c0f64bfdf20c64
jorge <egrojmonroy@gmail.com>
2017-04-20 00:07:44 -0400

    factura

---
+++ database/migrations/2017_04_20_023818_create_user_nit_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserNitTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::defaultStringLength(191);
+        Schema::create('user_nit',function (Blueprint $table ){
+            $table->increments('id');
+            $table->string('id_user');
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->integer('nit');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('type_salon');
+    }
+}
